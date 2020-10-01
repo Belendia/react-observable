cde73fa8563eaba9f5eb8f1b024a13e468d205a8
belendia <belendia@gmail.com>
2020-10-01 09:00:40 +0300

    Add auth reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,9 @@
-import { createStore } from "redux";
+import { createStore, compose } from "redux";
 import rootReducer from "./root.reducer";
 
-export default createStore(rootReducer);
+const composeEnhancers =
+  (process.env.NODE_ENV === "development"
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    : null) || compose;
+
+export default createStore(rootReducer, composeEnhancers());
